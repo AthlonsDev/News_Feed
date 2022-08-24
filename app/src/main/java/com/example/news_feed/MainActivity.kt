e2c5a503ceb8238d675d32eeea9b4ac06880b45f
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-08-24 15:27:55 +0100

    Added API Dependency and modified Gradle Setting file to PREFER_SETTINGS

--- app/src/main/java/com/example/news_feed/MainActivity.kt
+++ app/src/main/java/com/example/news_feed/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.news_feed
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kwabenaberko.newsapilib.NewsApiClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsApiClient = NewsApiClient("YOUR_API_KEY")
     }
 }
