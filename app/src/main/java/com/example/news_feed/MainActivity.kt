e9c64155830e6463ede1cbd2b01adf88dabba1ef
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-08-25 16:48:49 +0100

    Added Method to Retrieve Data From API

--- app/src/main/java/com/example/news_feed/MainActivity.kt
+++ app/src/main/java/com/example/news_feed/MainActivity.kt
@@ -3,6 +3,15 @@ package com.example.news_feed
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kwabenaberko.newsapilib.NewsApiClient
+import com.kwabenaberko.newsapilib.NewsApiClient.ArticlesResponseCallback
+import com.kwabenaberko.newsapilib.NewsApiClient.SourcesCallback
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest
+import com.kwabenaberko.newsapilib.models.request.SourcesRequest
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse
+import com.kwabenaberko.newsapilib.models.response.SourcesResponse
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +19,63 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val newsApiClient = NewsApiClient("YOUR_API_KEY")
+
+        getHeadlines(newsApiClient)
     }
+
+
+    private fun getHeadlines(newsApiClient: NewsApiClient) {
+        newsApiClient.getTopHeadlines(
+            TopHeadlinesRequest.Builder()
+                .q("Any Query")
+                .language("en")
+                .build(),
+            object : ArticlesResponseCallback {
+                override fun onSuccess(response: ArticleResponse) {
+                    println(response.articles[0].title)
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    println(throwable.message)
+                }
+            }
+        )
+    }
+
+    private fun getEverything(newsApiClient: NewsApiClient) {
+        newsApiClient.getEverything(
+            EverythingRequest.Builder()
+                .q("trump")
+                .build(),
+            object : ArticlesResponseCallback {
+                override fun onSuccess(response: ArticleResponse) {
+                    println(response.articles[0].title)
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    println(throwable.message)
+                }
+            }
+        )
+    }
+
+    private  fun getSources(newsApiClient: NewsApiClient) {
+        newsApiClient.getSources(
+            SourcesRequest.Builder()
+                .language("en")
+                .country("us")
+                .build(),
+            object : SourcesCallback {
+                override fun onSuccess(response: SourcesResponse) {
+                    println(response.sources[0].name)
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    println(throwable.message)
+                }
+            }
+        )
+    }
+
+
 }
