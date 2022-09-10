af33cb679dbe09d2b0a06561d6a5cc4936c1f34c
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-09-10 23:47:10 +0100

    Adding Web View

---
+++ app/src/main/java/com/example/news_feed/WebViewActivity.kt
@@ -0,0 +1,19 @@
+package com.example.news_feed
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WebViewActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.web_view_layout)
+
+    }
+
+
+
+}
